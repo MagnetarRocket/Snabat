b5541eb8fde1739902a0f6a004df72be727e733a
MagnetarRocket <53055079+MagnetarRocket@users.noreply.github.com>
2023-03-28 10:08:51 -0500

    added in the "meat" of the program

--- Snabat.c
+++ Snabat.c
@@ -1,6 +1,94 @@
-#include 
+#include<stdio.h>
+#include<stdlib.h> 
+#include<err.h>
+#include<time.h>
+//random > rand
+//© MagnetarRocket 2023
 
-Main()
+/*int Main(int argc, char **argsv)
 {
+	//need to dump in the file name from args
+	unsigned int fileLines = 0, lotsPick = 0;
+	char ;
+	//while(int bee <= argc, bee = 5, bee++)
+	if (argc > 1)
+	{}
+	else
+	{
 
+	}
+}*/
+//int Main(int argc, char **argsv)
+int Main(int argc, char *argsv[])
+{
+	//need to dump in the file name from args
+
+	FILE* dump;//
+	char blurt;
+	srandom(time(0));
+	//srand( time(0) );
+	/*while()
+	{}*/
+
+	int fileLines = 0, lotsPick = 0;
+	
+	/*while (EOF != (scanf ("%*[^\n]"), scanf("%[^\n]") ) )
+	{  
+		fileLines++; 
+	}
+
+	while (EOF != (fileLines= getchar()) )//
+	{
+		if(fileLines == '\n')
+		{
+			fileLines++;
+		}
+	}*/
+	
+	//char ;
+	for(int bee = 0; bee <= argsv; bee++)
+	{
+		dump = fopen(argsv[bee], "r");
+		if (dump == NULL)
+		printf("%s was not found, contuining on…", dump);
+		else
+		{
+			for (blurt = getc(dump);blurt != EOF;)
+			{
+				if (blurt == '\n')
+				{
+					fileLines++;
+				}
+			}
+		}
+		lotsPick = random() % fileLines;
+		printf();
+
+		fclose(dump);
+		fileLines = 0;
+	}
+	//while(int bee <= argc, bee = 5, bee++)
+	/*if (argc > 1)
+	{
+		printf ("Too much arguements, try one at an time.");
+	}
+	else
+	{
+
+	}*/
+	//fclose(dump);
+	return 0;
+}
+
+int ArgOptions(char inputC)
+{
+	switch (inputC)
+	{
+	case 'e':
+		/* code */
+		break;
+	
+	default:
+		break;
+	}
 }
